38229225f580a86178b49b98e8dc19c82326cf25
Simba-cheng <simba_cheng@163.com>
2019-02-18 19:39:29 +0800

    样式调整、编辑器新增动态修改背景、语言

--- src/main/resources/static/js/mario.js
+++ src/main/resources/static/js/mario.js
@@ -1,3 +1,5 @@
+var editor = "";
+
 var mario = {
 
     init: function () {
@@ -7,31 +9,53 @@ var mario = {
     },
 
     initMonaceEditor: function () {
-        //设置路径
-        require.config({paths: {'vs': '/static/monaco-editor/min/vs'}});
 
-        require(['vs/editor/editor.main'], function () {
-            var editor = monaco.editor.create(document.getElementById('paramContainer'), {
-                value: [
-                    '{',
-                    '  "properties": [',
-                    '    {',
-                    '      "defaultValue": true,',
-                    '      "name": "camel.component.jolt.enabled",',
-                    '      "description": "Enable jolt component",',
-                    '      "type": "java.lang.Boolean"',
-                    '    }',
-                    '  ]',
-                    '}'
-                ].join('\n'),
-                //支持语言
-                language: 'json',
-                //背景样式
-                theme: 'vs-dark',
-                //编辑器随浏览器窗口自动调整大小
-                automaticLayout: true,
-                //编辑器中文字的大小
-                fontSize: '16'
+        $(document).ready(function () {
+            //设置路径
+            require.config({paths: {'vs': '/static/monaco-editor/min/vs'}});
+
+            require(['vs/editor/editor.main'], function () {
+                mario.editor = monaco.editor.create(document.getElementById('paramContainer'), {
+                    value: [
+                        '{',
+                        '  "properties": [',
+                        '    {',
+                        '      "defaultValue": true,',
+                        '      "name": "camel.component.jolt.enabled",',
+                        '      "description": "Enable jolt component",',
+                        '      "type": "java.lang.Boolean"',
+                        '    }',
+                        '  ]',
+                        '}'
+                    ].join('\n'),
+                    //支持语言
+                    language: 'json',
+                    //背景样式
+                    theme: 'vs',
+                    //编辑器随浏览器窗口自动调整大小
+                    automaticLayout: true,
+                    //编辑器中文字的大小
+                    fontSize: '16'
+                });
+
+                //编辑器背景样式修改
+                $(".selectpicker-modifyEditorBackGround").change(function (e) {
+                    // console.log(e.target.value);
+                    // console.log(this.selectedIndex);
+                    changeTheme(this.selectedIndex);
+                });
+
+                //修改语言
+                try {
+                    $(".selectpicker-languageContent").change(function (e) {
+                        var name = e.target.value;
+                        var data = mario.editor.getValue();
+                        var newModel = monaco.editor.createModel(data, name);
+                        mario.editor.setModel(newModel);
+                    });
+                } catch (e) {
+                }
+
             });
         });
     },
@@ -46,7 +70,7 @@ var mario = {
     },
 
     //语言下拉框点击处理
-    lanagerSelectHandler:function () {
+    lanagerSelectHandler: function () {
 
         $("#languageSelectID").on('changed.bs.select', function (e) {
             console.log(e);
