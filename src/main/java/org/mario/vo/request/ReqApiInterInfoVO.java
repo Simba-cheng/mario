711ac621ab62b540e363eefe89ed6561737c0990
Simba_cheng <simba_cheng@163.com>
2020-09-21 22:50:52 +0800

    去除lombok组件

--- src/main/java/org/mario/vo/request/ReqApiInterInfoVO.java
+++ src/main/java/org/mario/vo/request/ReqApiInterInfoVO.java
@@ -1,7 +1,5 @@
 package org.mario.vo.request;
 
-import lombok.Data;
-
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -9,7 +7,6 @@ import javax.validation.constraints.NotNull;
  * @author CYX
  * @date: 2019/2/28 14:17
  */
-@Data
 @Valid
 public class ReqApiInterInfoVO {
 
@@ -75,4 +72,110 @@ public class ReqApiInterInfoVO {
      * 请求出参结构
      */
     private String responseParam;
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getApiNum() {
+        return apiNum;
+    }
+
+    public void setApiNum(String apiNum) {
+        this.apiNum = apiNum;
+    }
+
+    public String getApiName() {
+        return apiName;
+    }
+
+    public void setApiName(String apiName) {
+        this.apiName = apiName;
+    }
+
+    public String getApiPath() {
+        return apiPath;
+    }
+
+    public void setApiPath(String apiPath) {
+        this.apiPath = apiPath;
+    }
+
+    public String getApiURL() {
+        return apiURL;
+    }
+
+    public void setApiURL(String apiURL) {
+        this.apiURL = apiURL;
+    }
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public void setRequestMethod(String requestMethod) {
+        this.requestMethod = requestMethod;
+    }
+
+    public String getTerminal() {
+        return terminal;
+    }
+
+    public void setTerminal(String terminal) {
+        this.terminal = terminal;
+    }
+
+    public String getCoder() {
+        return coder;
+    }
+
+    public void setCoder(String coder) {
+        this.coder = coder;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getRequestParam() {
+        return requestParam;
+    }
+
+    public void setRequestParam(String requestParam) {
+        this.requestParam = requestParam;
+    }
+
+    public String getResponseParam() {
+        return responseParam;
+    }
+
+    public void setResponseParam(String responseParam) {
+        this.responseParam = responseParam;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ReqApiInterInfoVO{");
+        sb.append("projectName='").append(projectName).append('\'');
+        sb.append(", apiNum='").append(apiNum).append('\'');
+        sb.append(", apiName='").append(apiName).append('\'');
+        sb.append(", apiPath='").append(apiPath).append('\'');
+        sb.append(", apiURL='").append(apiURL).append('\'');
+        sb.append(", requestMethod='").append(requestMethod).append('\'');
+        sb.append(", terminal='").append(terminal).append('\'');
+        sb.append(", coder='").append(coder).append('\'');
+        sb.append(", product='").append(product).append('\'');
+        sb.append(", requestParam='").append(requestParam).append('\'');
+        sb.append(", responseParam='").append(responseParam).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
