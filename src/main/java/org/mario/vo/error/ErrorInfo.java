7d6acc7fbb15d1f2abb4986379786ffba9bc9da9
Simba_cheng <simba_cheng@163.com>
2020-09-21 22:50:19 +0800

    去除lombok组件

--- src/main/java/org/mario/vo/error/ErrorInfo.java
+++ src/main/java/org/mario/vo/error/ErrorInfo.java
@@ -1,12 +1,9 @@
 package org.mario.vo.error;
 
-import lombok.Data;
-
 /**
  * @author CYX
  * @create 2019-02-23-14:00
  */
-@Data
 public class ErrorInfo {
 
     /**
@@ -25,5 +22,30 @@ public class ErrorInfo {
     }
 
     public ErrorInfo() {
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ErrorInfo{");
+        sb.append("errorCode='").append(errorCode).append('\'');
+        sb.append(", errorMsg='").append(errorMsg).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
