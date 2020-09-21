1a84464727574075c04320f1b2f9ecf6bea48b29
Simba_cheng <simba_cheng@163.com>
2020-09-21 22:50:58 +0800

    去除lombok组件

--- src/main/java/org/mario/vo/request/ReqSaveParamDataVO.java
+++ src/main/java/org/mario/vo/request/ReqSaveParamDataVO.java
@@ -1,7 +1,5 @@
 package org.mario.vo.request;
 
-import lombok.Data;
-
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -9,7 +7,6 @@ import javax.validation.constraints.NotNull;
  * @author CYX
  * @date: 2019/2/28 22:30
  */
-@Data
 @Valid
 public class ReqSaveParamDataVO {
 
@@ -31,4 +28,37 @@ public class ReqSaveParamDataVO {
     @NotNull(message = "参数标示为空")
     private String paramFlag;
 
+    public String getApiName() {
+        return apiName;
+    }
+
+    public void setApiName(String apiName) {
+        this.apiName = apiName;
+    }
+
+    public String getParamData() {
+        return paramData;
+    }
+
+    public void setParamData(String paramData) {
+        this.paramData = paramData;
+    }
+
+    public String getParamFlag() {
+        return paramFlag;
+    }
+
+    public void setParamFlag(String paramFlag) {
+        this.paramFlag = paramFlag;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ReqSaveParamDataVO{");
+        sb.append("apiName='").append(apiName).append('\'');
+        sb.append(", paramData='").append(paramData).append('\'');
+        sb.append(", paramFlag='").append(paramFlag).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
