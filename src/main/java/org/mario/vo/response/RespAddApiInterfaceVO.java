1e28a8af2da03ac1eb8524dabd4ea809769d1939
Simba_cheng <simba_cheng@163.com>
2020-09-21 22:51:07 +0800

    去除lombok组件

--- src/main/java/org/mario/vo/response/RespAddApiInterfaceVO.java
+++ src/main/java/org/mario/vo/response/RespAddApiInterfaceVO.java
@@ -1,16 +1,29 @@
 package org.mario.vo.response;
 
-import lombok.Data;
-
 import java.util.List;
 
 /**
  * @author CYX
  * @date: 2019/2/28 14:50
  */
-@Data
+
 public class RespAddApiInterfaceVO extends ResponseVO {
 
     private List<RespApiInfoVO> apiInfos;
 
+    public List<RespApiInfoVO> getApiInfos() {
+        return apiInfos;
+    }
+
+    public void setApiInfos(List<RespApiInfoVO> apiInfos) {
+        this.apiInfos = apiInfos;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("RespAddApiInterfaceVO{");
+        sb.append("apiInfos=").append(apiInfos);
+        sb.append('}');
+        return sb.toString();
+    }
 }
